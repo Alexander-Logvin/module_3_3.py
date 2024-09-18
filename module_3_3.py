8e0a6c5c7ec533e015ba85bde9cb0b89e3dcc2ee
Alexander-Logvin <shura.logvin@yandex.ru>
2024-09-18 14:10:17 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,22 @@
+def print_params(a=1, b='строка', c=True):
+    print(a,b,c)
+
+print_params()
+print_params(a=2)
+print_params(b=25)
+c = [1,2,3]
+print_params(c)
+print_params(c)
+print_params(*c)
+print_params(*c)
+
+values_list = [25,True, 'String']
+print_params(b=values_list)
+print_params(*values_list)
+values_dict = {'a': 5,'b': True,'c': 'Urban'}
+print_params(**values_dict)
+print_params(c = values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
+print_params(values_list_2, 42)
